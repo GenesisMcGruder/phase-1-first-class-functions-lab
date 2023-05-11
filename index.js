7534a172bfe4a140ac4ae98f1ea22c747090479e
Genesis McGruder <genesismcgruder@gmail.com>
2023-05-11 00:18:29 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const firstTwoDrivers = drivers.slice(0,2);
+
+const lastTwoDrivers = drivers.slice(2)
+
+function returnFirstTwoDrivers(){
+    return firstTwoDrivers;
+}
+
+function returnLastTwoDrivers() {
+    return lastTwoDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multi) {
+    return function fareMultiplier(fare) {
+        return fare*multi
+    }
+}
+
+const fareDoubler = function(createFareMultiplier) {
+    return createFareMultiplier*2;
+}
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (drivers, select) {
+    return select(drivers);
+}
